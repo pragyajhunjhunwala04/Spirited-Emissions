56a4408ec3e899fcaf30f924e0d2b43ea0289942
Pragya Jhunjhunwala <pjhunjh1@uci.edu>
2025-04-12 16:46:16 -0700

    regex fixing

--- gpu_nn.py
+++ gpu_nn.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
-import regex
+import re
 
 CSV_FILE = "ai_ghibli_trend_dataset_v2.csv"
 
@@ -21,13 +21,26 @@ def remove_ghibli_style(prompts: list):
     """
     Given a list of prompts, extract duplication like "Ghibli" or "Anime-style"
     """
-    for prompt in prompts:
-        prompt = regex.sub(r'*Ghibli*', '', prompt)
-        prompt = regex.sub(r'*Anime*', '', prompt)
-        prompt = regex.sub(r'*inspired*', '', prompt)
-        prompt = regex.sub(r'*style *', '', prompt)
-    print(prompts)
-    return prompts
+    words_to_remove = ["ghibli", "anime", "inspired", "style"]
+    
+    modified_strings = []
+    
+    for string in prompts:
+        modified_string = string
+        
+        for word in words_to_remove:
+            pattern = re.compile(rf'\b{word}\b|-{word}|{word}-', re.IGNORECASE)
+            modified_string = pattern.sub('', modified_string)
+        
+        modified_string = re.sub(r'\s+', ' ', modified_string)
+        modified_string = re.sub(r'\s*-\s*', '-', modified_string)
+        modified_string = re.sub(r'-+', '-', modified_string)
+        modified_string = modified_string.strip('- ')
+        
+        modified_strings.append(modified_string)
+    
+    print(modified_strings)
+    return modified_strings
 
 
 def main():
