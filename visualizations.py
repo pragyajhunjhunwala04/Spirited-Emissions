9150cbfc6650bde6fff5573c4481510430f3440c
Deeksha Dwarakanath <dwarakad@uci.edu>
2025-04-12 19:01:51 -0700

    Finished with visualizations

--- visualizations.py
+++ visualizations.py
@@ -1,10 +1,11 @@
-import numpy
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import statsmodels.api as sm
+import seaborn as sns
 
 # Finding Missing Values
 df = pd.read_csv("ai_ghibli_trend_dataset_v2.csv")
+df = pd.DataFrame(df)
 missing_values = df.isnull().sum()
 print(missing_values)
 
@@ -27,4 +28,20 @@ plt.show()
 
 plt.boxplot(df['gpu_usage'])
 plt.title("GPU_Usage Boxplot")
+plt.show()
+
+grouped_values = [df[df['platform'] == cat]['gpu_usage'] for cat in df['platform'].unique()]
+labels = df['platform'].unique()
+# Plot stacked histogram
+plt.hist(grouped_values, bins=10, stacked=True, label=labels, edgecolor = 'black')
+plt.legend()
+plt.xlabel('GPU Usage * 100')
+plt.ylabel('Frequency')
+plt.title('Stacked Histogram of GPU Usage by Platform')
+plt.show()
+
+plt.scatter(df['gpu_usage'], df['generation_time'])
+plt.xlabel("Generation Time * 100")
+plt.ylabel("gpu_usage (percentage)")
+plt.title("Generation Time by GPU_Usage")
 plt.show()
