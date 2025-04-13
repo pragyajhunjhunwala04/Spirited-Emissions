b9a53d9634bde2e08df75e2a36c5a44f3d4954c2
Helena S <helenays@uci.edu>
2025-04-12 18:57:04 -0700

    Testing with regression models

--- gpu_nn.py
+++ gpu_nn.py
@@ -3,7 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
+from sklearn.linear_model import LinearRegression
 from sentence_transformers import SentenceTransformer
+from sklearn.svm import LinearSVR
+from sklearn.linear_model import SGDRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+
 import re
 
 CSV_FILE = "ai_ghibli_trend_dataset_v2.csv"
@@ -44,7 +51,10 @@ def remove_ghibli_style(prompts: list):
     return modified_strings
 
 
-def main():
+def regression():
+    """
+    MLP Regression model
+    """
     raw_numpy_array = extract_prompt_gpu()
     prompts_raw = raw_numpy_array[:,0]
     gpu_usage = raw_numpy_array[:,1].astype(float)
@@ -56,7 +66,101 @@ def main():
     learner.fit(X_train, Y_train)
     error = learner.score(X_test, Y_test)
     print(error)
-        
+
+
+
+def word_count():
+    raw_numpy_array = extract_prompt_gpu()
+    prompts_raw = raw_numpy_array[:,0]
+    gpu_usage = raw_numpy_array[:,1].astype(float)
+    prompts = remove_ghibli_style(prompts_raw.tolist())
+    X = np.array([[len(p)] for p in prompts])
+    X_train, X_test, Y_train, Y_test = train_test_split(X, gpu_usage, test_size=0.1, shuffle=True)
+    learner = MLPRegressor(learning_rate_init=0.005, max_iter=500, hidden_layer_sizes=70, batch_size=150)
+    learner.fit(X_train, Y_train)
+    error = learner.score(X_test, Y_test)
+    print(error)
+
+def linearreg():
+    raw_numpy_array = extract_prompt_gpu()
+    prompts_raw = raw_numpy_array[:,0]
+    gpu_usage = raw_numpy_array[:,1].astype(float)
+    prompts = remove_ghibli_style(prompts_raw.tolist())
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    X = model.encode(prompts)    
+    X_train, X_test, Y_train, Y_test = train_test_split(X, gpu_usage, test_size=0.1, shuffle=True)
+    learner = LinearRegression()
+    learner.fit(X_train, Y_train)
+    error = learner.score(X_test, Y_test)
+    print(error)
+
+def svm():
+    raw_numpy_array = extract_prompt_gpu()
+    prompts_raw = raw_numpy_array[:,0]
+    gpu_usage = raw_numpy_array[:,1].astype(float)
+    prompts = remove_ghibli_style(prompts_raw.tolist())
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    X = model.encode(prompts)    
+    X_train, X_test, Y_train, Y_test = train_test_split(X, gpu_usage, test_size=0.1, shuffle=True)
+    learner = LinearSVR()
+    learner.fit(X_train, Y_train)
+    error = learner.score(X_test, Y_test)
+    print(error)
+
+def sgd():
+    raw_numpy_array = extract_prompt_gpu()
+    prompts_raw = raw_numpy_array[:,0]
+    gpu_usage = raw_numpy_array[:,1].astype(float)
+    prompts = remove_ghibli_style(prompts_raw.tolist())
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    X = model.encode(prompts)    
+    X_train, X_test, Y_train, Y_test = train_test_split(X, gpu_usage, test_size=0.1, shuffle=True)
+    learner = SGDRegressor()
+    learner.fit(X_train, Y_train)
+    error = learner.score(X_test, Y_test)
+    print(error)
+
+def knn():
+    raw_numpy_array = extract_prompt_gpu()
+    prompts_raw = raw_numpy_array[:,0]
+    gpu_usage = raw_numpy_array[:,1].astype(float)
+    prompts = remove_ghibli_style(prompts_raw.tolist())
+    X = np.array([[len(p)] for p in prompts])
+    X_train, X_test, Y_train, Y_test = train_test_split(X, gpu_usage, test_size=0.1, shuffle=True)
+    learner = KNeighborsRegressor(n_neighbors=5)
+    learner.fit(X_train, Y_train)
+    error = learner.score(X_test, Y_test)
+    print(error)
+
+def tree():
+    raw_numpy_array = extract_prompt_gpu()
+    prompts_raw = raw_numpy_array[:,0]
+    gpu_usage = raw_numpy_array[:,1].astype(float)
+    prompts = remove_ghibli_style(prompts_raw.tolist())
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    X = model.encode(prompts)    
+    X_train, X_test, Y_train, Y_test = train_test_split(X, gpu_usage, test_size=0.1, shuffle=True)
+    learner = DecisionTreeRegressor()
+    learner.fit(X_train, Y_train)
+    error = learner.score(X_test, Y_test)
+    print(error)
+
+def boost():
+    raw_numpy_array = extract_prompt_gpu()
+    prompts_raw = raw_numpy_array[:,0]
+    gpu_usage = raw_numpy_array[:,1].astype(float)
+    prompts = remove_ghibli_style(prompts_raw.tolist())
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    X = model.encode(prompts)    
+    X_train, X_test, Y_train, Y_test = train_test_split(X, gpu_usage, test_size=0.1, shuffle=True)
+    learner = GradientBoostingRegressor()
+    learner.fit(X_train, Y_train)
+    error = learner.score(X_test, Y_test)
+    print(error)
+
+def main():
+    tree()
+    
 
 if __name__ == "__main__":
 
