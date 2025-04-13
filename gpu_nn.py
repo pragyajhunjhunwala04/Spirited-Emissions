8b36289a4981d535502072c58c433c4e92911cf6
Helena S <helenays@uci.edu>
2025-04-12 18:11:13 -0700

    Trying out MLPRegression

--- gpu_nn.py
+++ gpu_nn.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
+from sentence_transformers import SentenceTransformer
 import re
 
 CSV_FILE = "ai_ghibli_trend_dataset_v2.csv"
@@ -21,7 +22,7 @@ def remove_ghibli_style(prompts: list):
     """
     Given a list of prompts, extract duplication like "Ghibli" or "Anime-style"
     """
-    words_to_remove = ["ghibli", "anime", "inspired", "style"]
+    words_to_remove = ["ghibli", "anime", "inspired", "style", "studio"]
     
     modified_strings = []
     
@@ -39,15 +40,24 @@ def remove_ghibli_style(prompts: list):
         
         modified_strings.append(modified_string)
     
-    print(modified_strings)
+    # print(modified_strings)
     return modified_strings
 
 
 def main():
     raw_numpy_array = extract_prompt_gpu()
     prompts_raw = raw_numpy_array[:,0]
-    gpu_usage = raw_numpy_array[:,1]
+    gpu_usage = raw_numpy_array[:,1].astype(float)
     prompts = remove_ghibli_style(prompts_raw.tolist())
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    X = model.encode(prompts)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, gpu_usage, test_size=0.1, shuffle=True)
+    learner = MLPRegressor(learning_rate_init=0.005, max_iter=500, hidden_layer_sizes=100, batch_size=250)
+    learner.fit(X_train, Y_train)
+    error = learner.score(X_test, Y_test)
+    print(error)
+        
 
 if __name__ == "__main__":
+
     main()
